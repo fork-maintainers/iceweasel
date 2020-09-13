744c4f8005c6f1355e658a19149243996b989159
Abhijit Kiran Valluri <abhijitvalluri@users.noreply.github.com>
2020-09-13 15:26:06 +0100

    Revert "For #14145 - Add pinned badge for pinned sites"
    This reverts commit b8db505f1d609a02b50f75f04cf1eed138948e2d.

--- app/src/main/java/org/mozilla/fenix/home/sessioncontrol/viewholders/topsites/TopSiteItemViewHolder.kt
+++ app/src/main/java/org/mozilla/fenix/home/sessioncontrol/viewholders/topsites/TopSiteItemViewHolder.kt
@@ -16,7 +16,6 @@ import mozilla.components.browser.menu.item.SimpleBrowserMenuItem
 import mozilla.components.feature.top.sites.TopSite
 import mozilla.components.feature.top.sites.TopSite.Type.DEFAULT
 import mozilla.components.feature.top.sites.TopSite.Type.FRECENT
-import mozilla.components.feature.top.sites.TopSite.Type.PINNED
 import org.mozilla.fenix.R
 import org.mozilla.fenix.ext.components
 import org.mozilla.fenix.ext.loadIntoView
@@ -55,14 +54,8 @@ class TopSiteItemViewHolder(
     }
 
     fun bind(topSite: TopSite) {
+        this.topSite = topSite
         top_site_title.text = topSite.title
-
-        pin_indicator.visibility = if (topSite.type == PINNED) {
-            View.VISIBLE
-        } else {
-            View.GONE
-        }
-
         when (topSite.url) {
             SupportUtils.POCKET_TRENDING_URL -> {
                 favicon_image.setImageDrawable(getDrawable(itemView.context, R.drawable.ic_pocket))
@@ -71,8 +64,6 @@ class TopSiteItemViewHolder(
                 itemView.context.components.core.icons.loadIntoView(favicon_image, topSite.url)
             }
         }
-
-        this.topSite = topSite
     }
 
     private fun onTouchEvent(
